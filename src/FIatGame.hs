603d9a6524109e38461ffa13973854a8ffad9b8f
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-03-07 12:59:45 -0500

    api change

--- src/FIatGame.hs
+++ src/FIatGame.hs
@@ -8,6 +8,7 @@
 module FiatGame where
 
 import           Control.Lens
+import           Control.Monad.Except
 import           Data.Aeson
 import           Data.Aeson.TH
 import           Data.ByteString.Lazy (ByteString)
@@ -42,26 +43,28 @@ newtype FiatGameStateMsg = FiatGameStateMsg ByteString
 newtype FiatMoveMsg = FiatMoveMsg ByteString
   deriving (Eq,Show,Generic)
 
-class (ToJSON m, FromJSON m, ToJSON g, FromJSON g, ToJSON s, FromJSON s) => FiatGame g s m | g -> m, g -> s where
-  initial :: s -> FiatGameState g s m
-  makeMove :: FiatGameState g s m -> FiatMove m -> FiatGameState g s m
-  isPlayersTurn :: FiatGameState g s m -> FiatMove m -> Bool
-  isMoveValid :: FiatGameState g s m -> FiatMove m -> Bool
-  isMoveAuthorized :: FiatGameState g s m -> FiatPlayer -> FiatMove m -> Bool
-  isMoveAuthorized _ System _                                     = True
-  isMoveAuthorized _ (FiatPlayer _) (FiatMove System _)           = False
-  isMoveAuthorized _ (FiatPlayer p1) (FiatMove (FiatPlayer p2) _) = p1 == p2
-  processFromWebSocket :: FiatPlayer -> FiatGameStateMsg -> FiatMoveMsg -> Either FiatMoveError (FiatGameState g s m)
-  processFromWebSocket p (FiatGameStateMsg egs) (FiatMoveMsg emv) = do
-    gs <- over _Left (DecodeError . pack) $ eitherDecode egs
-    mv <- over _Left (DecodeError . pack) $ eitherDecode emv
-    boolToEither Unauthorized $ isMoveAuthorized gs p mv
+class (Monad m, ToJSON mv, FromJSON mv, ToJSON g, FromJSON g, ToJSON s, FromJSON s) => FiatGame m g s mv | s -> mv, s -> g where
+  initialSettings :: m s
+  addPlayer :: s -> FiatPlayer -> m (Maybe s)
+  initialGameState :: s -> m (FiatGameState g s mv)
+  makeMove :: FiatGameState g s mv -> FiatMove mv -> m (FiatGameState g s mv)
+  isPlayersTurn :: FiatGameState g s mv -> FiatMove mv -> m Bool
+  isMoveValid :: FiatGameState g s mv -> FiatMove mv -> m Bool
+  isMoveAuthorized :: FiatGameState g s mv -> FiatPlayer -> FiatMove mv -> m Bool
+  isMoveAuthorized _ System _                                     = return True
+  isMoveAuthorized _ (FiatPlayer _) (FiatMove System _)           = return False
+  isMoveAuthorized _ (FiatPlayer p1) (FiatMove (FiatPlayer p2) _) = return $ p1 == p2
+  processFromWebSocket :: FiatPlayer -> FiatGameStateMsg -> FiatMoveMsg -> m (Either FiatMoveError (FiatGameState g s mv))
+  processFromWebSocket p (FiatGameStateMsg egs) (FiatMoveMsg emv) = runExceptT $ do
+    gs <- ExceptT $ return $ over _Left (DecodeError . pack) $ eitherDecode egs
+    mv <- ExceptT $ return $ over _Left (DecodeError . pack) $ eitherDecode emv
+    ExceptT $ boolToEither Unauthorized <$> isMoveAuthorized gs p mv
     let isSystem = case mv of
                     (FiatMove System _) -> True
                     _                   -> False
-    boolToEither NotYourTurn $ isSystem || isPlayersTurn gs mv
-    boolToEither Invalid $ isMoveValid gs mv
-    return $ makeMove gs mv
+    ExceptT $ boolToEither NotYourTurn . (isSystem ||) <$> isPlayersTurn gs mv
+    ExceptT $ boolToEither Invalid <$> isMoveValid gs mv
+    lift $ makeMove gs mv
 
 boolToEither :: a -> Bool -> Either a ()
 boolToEither _ True  = Right ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,38 +5,58 @@
 {-# LANGUAGE TemplateHaskell       #-}
 
 import           Control.Monad
+import           Control.Monad.Identity
+import           Control.Monad.Trans.Maybe
 import           Data.Aeson
 import           Data.Aeson.TH
-import           Data.ByteString.Lazy (ByteString)
-import           Data.Text            (Text)
+import           Data.ByteString.Lazy      (ByteString)
+import           Data.Text                 (Text)
 import           FiatGame
 import           Test.Hspec
-
 
 data NoGame = A | B
   deriving (Eq, Show)
 $(deriveJSON defaultOptions ''NoGame)
 data NoSettings = NoSettings
+  { players :: [FiatPlayer]
+  }
   deriving (Eq, Show)
 $(deriveJSON defaultOptions ''NoSettings)
 data NoMoves = ToA | ToB
   deriving (Eq, Show)
 $(deriveJSON defaultOptions ''NoMoves)
-instance FiatGame NoGame NoSettings NoMoves where
-  makeMove (FiatGameState A s _) _ = FiatGameState B s []
-  makeMove (FiatGameState B s _) _ = FiatGameState A s []
-  isMoveValid (FiatGameState A _ _) (FiatMove _ ToB) = True
-  isMoveValid (FiatGameState B _ _) (FiatMove _ ToA) = True
-  isMoveValid _ _                                    = False
-  isPlayersTurn (FiatGameState A _ _) (FiatMove (FiatPlayer 0) _)  = True
-  isPlayersTurn (FiatGameState B _ _)  (FiatMove (FiatPlayer 1) _) = True
-  isPlayersTurn _ _                                                = False
-  initial s = FiatGameState A s []
 
-type NoGameFiatGameState = Either FiatMoveError (FiatGameState NoGame NoSettings NoMoves)
+instance FiatGame Identity NoGame NoSettings NoMoves where
+  initialSettings = return initSettings
+  addPlayer (NoSettings ps) p
+    | length ps < 2 = return $ Just (NoSettings (p:ps))
+    | otherwise = return Nothing
+  makeMove (FiatGameState A s _) _ = return $ FiatGameState B s []
+  makeMove (FiatGameState B s _) _ = return $ FiatGameState A s []
+  isMoveValid (FiatGameState A _ _) (FiatMove _ ToB) = return True
+  isMoveValid (FiatGameState B _ _) (FiatMove _ ToA) = return True
+  isMoveValid _ _                                    = return False
+  isPlayersTurn (FiatGameState A _ _) (FiatMove (FiatPlayer 0) _)  = return True
+  isPlayersTurn (FiatGameState B _ _)  (FiatMove (FiatPlayer 1) _) = return True
+  isPlayersTurn _ _                                                = return False
+  initialGameState s = return $ FiatGameState A s []
+
+type NoGameFiatGameState = Identity (Either FiatMoveError (FiatGameState NoGame NoSettings NoMoves))
+
+initSettings :: NoSettings
+initSettings = NoSettings []
+
+goodSettings :: Maybe NoSettings
+goodSettings = runIdentity $ do
+  i <- initialSettings
+  runMaybeT $ foldM (\s p -> MaybeT $ addPlayer s p) i [FiatPlayer 0, FiatPlayer 1]
+badSettings :: Maybe NoSettings
+badSettings = runIdentity $ do
+    i <- initialSettings
+    runMaybeT $ foldM (\s p -> MaybeT $ addPlayer s p) i [FiatPlayer 0, FiatPlayer 1, FiatPlayer 2]
 
 initialState :: FiatGameStateMsg
-initialState = FiatGameStateMsg $ encode (FiatGameState A NoSettings [] :: FiatGameState NoGame NoSettings NoMoves)
+initialState = FiatGameStateMsg $ encode (FiatGameState A initSettings [] :: FiatGameState NoGame NoSettings NoMoves)
 systemMove :: FiatMoveMsg
 systemMove = FiatMoveMsg $ encode $ FiatMove System ToB
 goodMove :: FiatMoveMsg
@@ -47,20 +67,24 @@ unauthorizedMove :: FiatMoveMsg
 unauthorizedMove = FiatMoveMsg $ encode $ FiatMove (FiatPlayer 1) ToB
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
   describe "processFromWebSocket" $ do
     it "good"
-      $ (processFromWebSocket (FiatPlayer 0) initialState goodMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B NoSettings [])
+      $ runIdentity (processFromWebSocket (FiatPlayer 0) initialState goodMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B initSettings [])
     it "system allowed"
-      $ (processFromWebSocket System initialState systemMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B NoSettings [])
+      $ runIdentity (processFromWebSocket System initialState systemMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B initSettings [])
     it "system allowed to move on other's behalf"
-      $ (processFromWebSocket System initialState goodMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B NoSettings [])
+      $ runIdentity (processFromWebSocket System initialState goodMove :: NoGameFiatGameState) `shouldBe` Right (FiatGameState B initSettings [])
     it "unauthorized"
-      $ (processFromWebSocket (FiatPlayer 0) initialState unauthorizedMove :: NoGameFiatGameState) `shouldBe` Left Unauthorized
+      $ runIdentity (processFromWebSocket (FiatPlayer 0) initialState unauthorizedMove :: NoGameFiatGameState) `shouldBe` Left Unauthorized
     it "not your turn"
-      $ (processFromWebSocket (FiatPlayer 1) initialState unauthorizedMove :: NoGameFiatGameState) `shouldBe` Left NotYourTurn
+      $ runIdentity (processFromWebSocket (FiatPlayer 1) initialState unauthorizedMove :: NoGameFiatGameState) `shouldBe` Left NotYourTurn
     it "invalid"
-      $ (processFromWebSocket (FiatPlayer 0) initialState invalidMove :: NoGameFiatGameState) `shouldBe` Left Invalid
+      $ runIdentity (processFromWebSocket (FiatPlayer 0) initialState invalidMove :: NoGameFiatGameState) `shouldBe` Left Invalid
     it "decode error"
-      $ (processFromWebSocket (FiatPlayer 1) (FiatGameStateMsg "") (FiatMoveMsg "") :: NoGameFiatGameState) `shouldBe` Left (DecodeError "Error in $: not enough input")
-
+      $ runIdentity (processFromWebSocket (FiatPlayer 1) (FiatGameStateMsg "") (FiatMoveMsg "") :: NoGameFiatGameState) `shouldBe` Left (DecodeError "Error in $: not enough input")
+  describe "addPlayer" $ do
+    it "good"
+      $ goodSettings `shouldBe` Just (NoSettings [FiatPlayer 1, FiatPlayer 0])
+    it "game is full"
+      $ badSettings `shouldBe` Nothing
