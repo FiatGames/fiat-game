515b2a741388c77bb94172c565cff5e602f02746
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-03-15 16:41:30 -0400

    name change

--- src/FiatGame/ToClient/Types.hs
+++ src/FiatGame/ToClient/Types.hs
@@ -14,7 +14,7 @@ import           Data.Text          (Text)
 import           FiatGame.GameState
 import           GHC.Generics
 
-data Error = GameIsNotStarted | GameAlreadyStarted | InvalidMove | Unauthorized | NotYourTurn | NotEnoughFiatPlayers | DecodeError Text | FailedToInitialize Text
+data Error = GameIsNotStarted | GameAlreadyStarted | InvalidMove | Unauthorized | NotYourTurn | NotEnoughPlayers | DecodeError Text | FailedToInitialize Text
   deriving (Eq,Show,Generic)
 $(deriveJSON defaultOptions ''Error)
 
